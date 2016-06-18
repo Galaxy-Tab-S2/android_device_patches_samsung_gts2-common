181920cbaf772729b4065bb83e696710ad8c19fd
raymanfx <raymanfx@gmail.com>
2016-06-19 01:33:03 +0200

    selinux: Disable selinux enforcing for now
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- patch.sh
+++ patch.sh
@@ -2,6 +2,12 @@
 LOCAL_PATH=$(dirname $(readlink -f $0))
 TOP=$LOCAL_PATH/../../../..
 
+function make_patch_stamp
+{
+	echo "$1" > $2/.patchlist
+	echo "gts2-common" > $2/.patch-device
+}
+
 if [ $(cat ${LOCAL_PATH}/PATCHED) = "0" ]; then
 
 	echo "Applying gts2-common patches"
@@ -10,5 +16,19 @@ if [ $(cat ${LOCAL_PATH}/PATCHED) = "0" ]; then
 
 	# Copy gts28wifi defconfig into kernel
 	cp ${LOCAL_PATH}/patched/kernel/samsung/gts2/arch/arm/configs/cm_exynos5433-gts28wifi_defconfig ${TOP}/kernel/samsung/gts2/arch/arm/configs/cm_exynos5433-gts28wifi_defconfig
+
+	# Disable SELINUX
+	make_patch_stamp 'init.cpp' ${TOP}/system/core/init/
+
+	if [ ! -d ${TOP}/hardware/samsung_slsi-cm/exynos5433/include/.backup ]; then
+
+		mkdir ${TOP}/system/core/init/.backup
+		echo "THIS DIR CONTAINS UNTOUCHED/UNPATCHED FILES, DO NOT REMOVE! THIS DIR WILL GET REMOVED AUTOMATICALLY IF NECCESSARY" > ${TOP}/system/core/init/.backup/README
+
+		mv ${TOP}/system/core/init/init.cpp ${TOP}/system/core/init/.backup/init.cpp.backup
+
+	fi
+
+	cp ${LOCAL_PATH}/patched/system/core/init/init.cpp ${TOP}/system/core/init
 
 fi
--- revert.sh
+++ revert.sh
@@ -3,12 +3,24 @@ LOCAL_PATH=$(dirname $(readlink -f $0))
 TOP=$LOCAL_PATH/../../../..
 
 if [ $(cat ${LOCAL_PATH}/PATCHED) = "1" ]; then
-    
-    echo "NOT BUILDING gts2-common! REVERTING gts2-common PATCHES!"
+
+	echo "NOT BUILDING gts2-common! REVERTING gts2-common PATCHES!"
 
 	echo 0 > ${LOCAL_PATH}/PATCHED
 
 	# This isn't neccessary but we'll do it anyways just to clear things up
 	rm ${TOP}/kernel/samsung/gts2/arch/arm/configs/cm_exynos5433-gts28wifi_defconfig
-   
+
+	if (grep -Fq "init.cpp" ${TOP}/system/core/init/.patchlist); then
+
+   		if [ $(cat ${TOP}/system/core/init/.patch-device) = "gts2-common" ]; then
+
+			mv ${TOP}/system/core/init/.backup/init.cpp.backup ${TOP}/system/core/init/init.cpp
+
+			rm -R ${TOP}/system/core/init/.backup
+			rm ${TOP}/system/core/init/.patchlist
+			rm ${TOP}/system/core/init/.patch-device
+
+		fi
+	fi
 fi
